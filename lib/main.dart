19b18ef8371c4fc2ca2c6b81dafcc0ce7e52a1d8
Jadenw9013 <130799431+Jadenw9013@users.noreply.github.com>
2025-07-23 22:32:13 -0700

    Add Firebase auth & Firestore user signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+
+}
 //hello guys
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
